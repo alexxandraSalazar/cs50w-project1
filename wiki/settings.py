2d4ebee100f328d745358e49c8f4e442e88dcd65
Alexandra Salazar <138623509+Alex000127@users.noreply.github.com>
2025-07-18 17:56:16 -0600

    :rocket:2

--- wiki/settings.py
+++ wiki/settings.py
@@ -23,8 +23,9 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
 SECRET_KEY = '%710m*zic)#0u((qugw#1@e^ty!c)9j04956v@ly(_86n$rg)h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
+ALLOWED_HOSTS = ['cs50w-project1-wiki.onrender.com', 'localhost', '127.0.0.1']
 
 
 
